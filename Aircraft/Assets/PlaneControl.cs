11830ccc1966ea1674d6e2f942f6a253d400660b
SaberGW <pawel.dziewanowski@onet.pl>
2015-02-23 12:00:54 +0100

    Aircraft 001
    Aircraft Sim basic movement

---
+++ Aircraft/Assets/PlaneControl.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaneControl : MonoBehaviour
+{
+
+	public float AmbientSpeed;
+	public float RotationSpeed;
+
+	// Use this for initialization
+	void Start ()
+	{
+		AmbientSpeed = 0f;
+		RotationSpeed = 0f;
+	}
+
+	void FixedUpdate ()
+	{
+		UpdateFunction ();
+	}
+
+	void UpdateFunction ()
+	{
+		Quaternion AddRotation = Quaternion.identity;
+
+		float roll = 0;
+		float pitch = 0;
+		float yaw = 0;
+
+		roll = Input.GetAxis ("Roll") * (Time.deltaTime * RotationSpeed);
+		pitch = Input.GetAxis ("Pitch") * (Time.deltaTime * RotationSpeed);
+		yaw = Input.GetAxis ("Yaw") * (Time.deltaTime * RotationSpeed);
+
+
+		AddRotation.eulerAngles = new Vector3 (-pitch, yaw, - roll);
+		rigidbody.rotation *= AddRotation;
+	}
+}
---
+++ Aircraft/Assets/PlanePilot.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanePilot : MonoBehaviour
+{
+
+	public float speed = 100.0f;
+
+	// Use this for initialization
+
+
+	void Start ()
+	{
+		Debug.Log ("plane pilot script added to: " + gameObject.name);
+		Debug.Log (Vector3.right);
+		Debug.Log (Vector3.left);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		Vector3 moveCamTo = transform.position - transform.forward * 10.0f + Vector3.up * 5.0f;
+		float bias = 0.80f;
+
+		Camera.main.transform.position = Camera.main.transform.position * bias +
+			moveCamTo * (1.0f - bias);
+		Camera.main.transform.LookAt (transform.position + transform.forward * 30.0f);
+
+		transform.position += transform.forward * Time.deltaTime * speed;
+
+		speed -= transform.forward.y * Time.deltaTime * 50.0f;
+
+		if (speed < 35.0f) {
+			speed = 35.0f;
+		}
+
+
+
+
+			
+
+
+		transform.Rotate (Input.GetAxis ("Vertical"), 0.0f, - Input.GetAxis ("Horizontal") * 3.0f);
+
+		float terrainHeightAtPlanePosition = Terrain.activeTerrain.SampleHeight (transform.position);
+
+		if (terrainHeightAtPlanePosition > transform.position.y) {
+			transform.position = new Vector3 (transform.position.x,
+			                                 terrainHeightAtPlanePosition,
+			                                 transform.position.z);
+		}
+	}
+}
